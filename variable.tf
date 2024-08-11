18bdf7a0775af706b925a8f97ba344c819d08795
Maresh <73285206+Mareshnani123@users.noreply.github.com>
2024-08-11 18:34:29 +0530

    Create variable.tf

---
+++ variable.tf
@@ -0,0 +1,8 @@
+variable "resource_group_name" {
+    description="resource group name will be updarted here"
+  
+}
+variable "location" {
+    description = "location at which the resource has to be provisioned"
+  
+}
