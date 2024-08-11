e84109d71b18f4b347ac3429013c75e919d9be13
Maresh <73285206+Mareshnani123@users.noreply.github.com>
2024-08-11 18:44:34 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -4,5 +4,8 @@ variable "resource_group_name" {
 }
 variable "location" {
     description = "location at which the resource has to be provisioned"
+}
+variable "container_name" {
+    default = "name of the container defined here"
   
 }
